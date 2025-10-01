01b4bbb7293c587bae6d35002debd1661baa244d
Miguel Gordillo <elloquius@gmail.com>
2025-10-01 15:23:08 -0400

    Se agregan comentarios para mejorar la comprension del archivo

--- main.py
+++ main.py
@@ -9,26 +9,27 @@ from utils.config import config
 from utils.predict_manual import predict_manual
 from utils.predict_from_csv import predict_from_csv
 
-# Debugging configuración
+# Debugging configuración del modelo (imprime los parámetros clave)
 print("Configuración del modelo:")
 for key, value in config.items():
     print(f"  {key}: {value}")
 
 # ===============================
-# ENTRENAMIENTO
+# ENTRENAMIENTO (Usando las diferentes funciones y clases definidas en los otros archivos)
 # ===============================
 X_train, y_train, X_test, y_test = preprocess_data(config["data_path"])
 input_size = X_train.shape[1]
 model = FeedforwardNN(input_size, config["hidden_size"], config["output_size"])
 
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"])
+criterion = torch.nn.MSELoss() # Función de pérdida (MSE para regresión)
+optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"]) # Optimizador Adam con la tasa de aprendizaje del config
 
-train_model(model, X_train, y_train, criterion, optimizer,
+train_model(model, X_train, y_train, criterion, optimizer, # Entrena el modelo
             config["epochs"], config["batch_size"])
 
 with torch.no_grad():
-    predictions = model(X_test)
+    predictions = model(X_test) # Estamos pasando datos de entrada (X_test) a través del modelo para obtener una predicción. Internamente,
+                                # esto ejecuta el método forward que se definió en la clase FeedforwardNN.
     test_loss = criterion(predictions, y_test).item()
     print(f"\nMSE en test: {test_loss:.4f}")
 
@@ -40,12 +41,14 @@ torch.save(model.state_dict(), model_path)
 print(f"Modelo guardado en: {model_path}")
 
 # ===============================
-# CARGAR MODELO
+# CARGAR MODELO ENTRENADO Y PROBAR
 # ===============================
 print("\nProbando el modelo guardado...")
-loaded_model = FeedforwardNN(input_size, config["hidden_size"], 1)
-loaded_model.load_state_dict(torch.load(model_path))
-loaded_model.eval()
+loaded_model = FeedforwardNN(input_size, config["hidden_size"], 1) # Reconstruye el modelo con la misma arquitectura.
+loaded_model.load_state_dict(torch.load(model_path))  # Carga los pesos guardados.
+loaded_model.eval()   # Pone el modelo en modo evaluación (eval()).
+
+# Verifica que el modelo cargado funciona igual que el original
 with torch.no_grad():
     loaded_predictions = loaded_model(X_test)
     loaded_test_loss = criterion(loaded_predictions, y_test).item()
