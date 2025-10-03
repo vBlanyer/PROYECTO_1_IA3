e5b61b6be674eb385d04c550ef4c4bd1123b803d
Miguel Gordillo <elloquius@gmail.com>
2025-10-03 14:10:40 -0400

    Se actualizan las configuraciones

--- utils/config.py
+++ utils/config.py
@@ -1,11 +1,12 @@
 # Configuraciones clave para el proyecto, como rutas de datos, tamaños de capas de la red, tasa de aprendizaje, etc.
 config = {
-      "data_path": "data/Popular_Spotify_Songs.csv",
-      "input_size": 10,
+      "data_path": "data/dataset.csv",
+      "data_path_cleaned": "data/dataset_cleaned.csv",
+      "input_size": 18,  # Esto no se esta usando, ya que el input se autoprocesa en el preprocessor_data dependiendo de las columnas que encuentre
       "hidden_size": 64,
       "output_size": 1,
       "batch_size": 32,
       "learning_rate": 0.001,
-      "epochs": 10000,
-      "test_path": "data/Popular_Spotify_Songs.csv"
+      "epochs": 300,
+      "test_path": "data/dataset_cleaned.csv"
 }
