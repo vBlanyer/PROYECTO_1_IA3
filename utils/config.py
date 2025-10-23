b145bac774cc75fdb45dc2a092e80be1d38c609d
Miguel Gordillo <elloquius@gmail.com>
2025-10-22 21:56:52 -0400

    Configuracion final de la red

--- utils/config.py
+++ utils/config.py
@@ -1,12 +1,12 @@
 # Configuraciones clave para el proyecto, como rutas de datos, tamaños de capas de la red, tasa de aprendizaje, etc.
 config = {
-      "data_path": "data/dataset.csv",
-      "data_path_cleaned": "data/dataset_cleaned.csv",
+      "data_path": "data/spotify_tracks_updated.csv",
+      "data_path_cleaned": "data/spotify_tracks_updated.csv",
       "input_size": 18,  # Esto no se esta usando, ya que el input se autoprocesa en el preprocessor_data dependiendo de las columnas que encuentre
-      "hidden_size": 64,
+      "hidden_size": 128,
       "output_size": 1,
       "batch_size": 32,
       "learning_rate": 0.001,
-      "epochs": 300,
-      "test_path": "data/dataset_cleaned.csv"
+      "epochs": 200,
+      "test_path": "data/spotify_tracks_updated.csv"
 }
